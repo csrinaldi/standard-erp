53cff5fb6d7e844302f968c33d1463387ce6cefa
Andrés Testi <andres.a.testi@gmail.com>
2012-08-06 18:27:13 -0300

    Caso de uso system.User

--- kratos/kratos-core/src/main/java/com/logikas/kratos/system/shared/factory/SystemRequestFactory.java
+++
@@ -1,8 +0,0 @@
-package com.logikas.kratos.system.shared.factory;
-
-import com.google.web.bindery.requestfactory.shared.RequestFactory;
-
-public interface SystemRequestFactory extends RequestFactory {
-  
-  UserServiceRequestContext createUserService();  
-}
